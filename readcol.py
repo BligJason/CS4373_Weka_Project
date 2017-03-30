8570332e1023ca5bee37b1ff7aecf5cf148b12ff
nicholas birchfield <nicholas birchfield>
2017-03-30 11:21:40 -0600

    outputs champions played in at least 20% of games

--- readcol.py
+++ readcol.py
@@ -1,4 +1,5 @@
 import csv
+
 with open("d:datamining/datasets/datasets/dota2Dataset/dota2Test.csv", 'r') as file:
 	reader = csv.reader(file)
 	first = next(reader)
@@ -13,6 +14,16 @@ with open("d:datamining/datasets/datasets/dota2Dataset/dota2Test.csv", 'r') as f
 			if int(item) == 1 or int(item) == -1:
 				count[index] += 1
 			index += 1
-	print(count)
-	
-			
+	with open("d:datamining/datasets/datasets/dota2Dataset/output.txt", 'w+') as output:		
+		pop_champs = dict()
+		index = 0
+		for item in first:
+			pop_champs[item] = count[int(index)]
+			index += 1
+		output.write("Champion GamesPlayed\n")	
+		for item in reversed(sorted(pop_champs.items(), key=lambda x:x[1])):
+			if item[1] >= pop_champs['radiant_victory_yes_no']*.2:
+				if item[1] == pop_champs['radiant_victory_yes_no']:
+					continue
+				output.write(" ".join(map(str, item)))
+				output.write("\n")
